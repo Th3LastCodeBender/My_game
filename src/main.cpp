fa2430d98eda4d2400153bb5b8524ff27996e206
Th3LastCodeBender <parolisi.leonardo@gmail.com>
2026-06-09 00:20:21 +0200

    [1;31mFEATURE: [1;38;5;208mWORK IN PROGRESS[0m[0m | Creating a parseShader function that automatically takes shader function source code from aa shadefile | res/shaders/shader.shader, src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,44 @@
 
 t_Engine engine;
 
+// static std::string *parseShader(const std::string &filepath, std::string type)
+// {
+// 	std::ifstream		stream(filepath);
+// 	std::string			strFuncts[2];
+// 	std::string			line;
+// 	std::stringstream 	stringStream;
+// 	int					attribute;
+
+// 	attribute = -1;
+// 	while (getline(stream, line))
+// 	{
+// 		if (attribute != -1)
+// 		{
+// 			stringStream << line;
+// 			if (line.empty() || line[0] == '\0' || line.find("};") != std::string::npos)
+// 			{
+// 				strFuncts[attribute] = stringStream.str();
+// 				stringStream.clear();
+// 				attribute = -1;		
+// 			}
+// 			continue;
+// 		}
+// 		if (line.find('#' + type) != std::string::npos)
+// 		{
+// 			if (line.find('#' + type + "fragment shader") != std::string::npos)
+// 				attribute = VERTEX;
+// 			else if (line.find('#' + type + "vertex shader") != std::string::npos)
+// 				attribute = FRAGMENT;
+// 		}
+// 	}
+// 	if (strFuncts[0].empty() || strFuncts[0][0] == '\0' || strFuncts[1].empty() || strFuncts[1][0] == '\0')
+// 		std::cerr	<< "[parseShader]: Something went wrong while parsing the " 
+// 					<< type 
+// 					<< " shader function!" 
+// 					<< std::endl;
+// 	return (strFuncts);
+// }
+
 static unsigned int compileShader(unsigned int type, const std::string &source)
 {
 	unsigned int	id;
@@ -76,24 +114,6 @@ int main()
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-
-	std::string vertexShader =	"#version 410 core\n"
-								"\n"
-								"layout(location = 0) in vec4 position;\n"
-								"\n"
-								"void main()\n"
-								"{\n"
-								"	gl_Position = position;\n"
-								"}\n";
-
-	std::string fragmentShader = "#version 410 core\n"
-								"\n"
-								"layout(location = 0) out vec4 color;\n"
-								"\n"
-								"void main()\n"
-								"{\n"
-								"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-								"}\n";
 
 	unsigned int shader = createShader(vertexShader, fragmentShader);
 	glUseProgram(shader);
